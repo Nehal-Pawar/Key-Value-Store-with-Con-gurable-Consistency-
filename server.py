ffe4f605e235e2fdea948abffc453c579d8e9e08
vkudalk1 <vkudalk1@binghamton.edu>
2018-11-30 19:18:34 -0500

    ByteOrderPartitioner for get v1

--- server.py
+++ server.py
@@ -17,13 +17,46 @@ from thrift.server import TServer
 
 
 store = {}
+replica_name = []
+replicas = {}
+
+
+
+
 
 class StoreHandler():
 
 	def get(self, key):
 		print 'get'
-		print store[key]
-		return store[key]
+		response = [-1,-1,-1]
+		if key>=0 and key <= 63:
+			for i in range(0,3):
+				if replica_name[i] != sys.argv[1]:
+					transport = TSocket.TSocket(replicas[replica_name[i]][0], replicas[replica_name[i]][1])
+					transport = TTransport.TBufferedTransport(transport)
+					protocol = TBinaryProtocol.TBinaryProtocol(transport)
+					client = FileStore.Client(protocol)
+
+					transport.open()
+					response[i]= client.getIN(key)
+					transport.close()
+
+				else:
+					response[i] = store[key]
+
+			print response
+			#rpc 0, 1 and 2
+
+		elif key>=64 and key <=127:
+			#rpc 1, 2 and 3
+		elif key>=128 and key <= 191:
+			#rpc 2, 3 and 0
+		else:
+			#rpc 3, 0 and 1
+
+		# compare TS of replicas 1,2,3 return key of max TS replica
+		#print store[key]
+		return max(response)
 
 	def put(self, keyvalue):
 		walfile = sys.argv[1] + 'wal'
@@ -49,14 +82,34 @@ class StoreHandler():
 			print 'put'
 			print store
 
+
+	def getIN(self,key):
+		#print store[key]
+		return store[key]
+
 if __name__ == '__main__':
 	# No command line arguments needed
 	if len(sys.argv) != 5:
 		print("Usage:", sys.argv[0], "Branch name", "Port number", "WAL file", "nodes.txt")
 		sys.exit()
 
-
+	# IP address of replica
 	print socket.gethostbyname(socket.gethostname())
+
+	#Read nodes file
+	with open(nodes) as f:
+		for line in f:
+			name = line.split()[0]
+			replica_name.append(name)
+			temp_list = []
+			temp_list.append(line.split()[1])
+			temp_list.append(line.split()[2])
+			replicas[name] = temp_list
+
+
+	
+	
+	
 	WAL = sys.argv[3]
 
 	# if write-ahead log file present
@@ -71,7 +124,7 @@ if __name__ == '__main__':
 				# later lock this store
 				store[key] = value
 				
-
+		f.close()
 		print store
 
 	else:
